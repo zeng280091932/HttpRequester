8ef4313ad3f7fb8332f7d4606c0fb5f56bb823df
zengjiantao <280091932@qq.com>
2020-08-07 17:35:59 +0800

    增加数据并保存下载信息，支持下载暂停功能

--- objectdb/src/main/java/com/beauney/objectdb/BaseDaoFactory.java
+++ objectdb/src/main/java/com/beauney/objectdb/BaseDaoFactory.java
@@ -3,6 +3,11 @@ package com.beauney.objectdb;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Environment;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zengjiantao
  * @since 2020-07-31
@@ -12,6 +17,8 @@ public class BaseDaoFactory {
 
     private SQLiteDatabase mSQLiteDatabase;
 
+    private Map<String, BaseDao> mMap = Collections.synchronizedMap(new HashMap<String, BaseDao>());
+
     private static BaseDaoFactory sInstance = new BaseDaoFactory();
 
     public static BaseDaoFactory getInstance() {
@@ -19,7 +26,11 @@ public class BaseDaoFactory {
     }
 
     private BaseDaoFactory() {
-        mSQLiteDatabasePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/download.db";
+        File file = new File(Environment.getExternalStorageDirectory(), "Databases");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        mSQLiteDatabasePath = file.getAbsolutePath() + "/download.db";
         openDatabase();
     }
 
@@ -27,14 +38,18 @@ public class BaseDaoFactory {
         mSQLiteDatabase = SQLiteDatabase.openOrCreateDatabase(mSQLiteDatabasePath, null);
     }
 
-    public synchronized <T extends BaseDao<M>,M> T getDataHelper(Class<T> clazz, Class<M> entityClazz) {
+    public synchronized <T extends BaseDao<M>, M> T getDataHelper(Class<T> clazz, Class<M> entityClazz) {
         BaseDao baseDao = null;
+        if (mMap.get(clazz.getSimpleName()) != null) {
+            return (T) mMap.get(clazz.getSimpleName());
+        }
         try {
             baseDao = clazz.newInstance();
             baseDao.init(entityClazz, mSQLiteDatabase);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            mMap.put(clazz.getSimpleName(), baseDao);
         } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
         return (T) baseDao;
